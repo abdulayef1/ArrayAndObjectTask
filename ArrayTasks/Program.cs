fa23af31cdbd4fa92644a2261695003038494683
abdulayef1 <7ka1cy@.code.edu.az>
2022-11-01 20:40:48 +0400

    ArrayTasks created

---
+++ ArrayTasks/Program.cs
@@ -0,0 +1,52 @@
+﻿#region task1
+////Ədədlərdən ibarət olan, verilmiş arrayin ədədi ortasını tapın.
+
+//double[] numberArr = { 1, 2, 3, 4, 5 };
+//double sum = 0;
+//double averageNumber;
+
+//for (int i = 0; i < numberArr.Length; i++)
+//{
+//    sum = numberArr[i] + sum;
+//}
+
+//double average = sum / numberArr.Length; 
+//Console.WriteLine(average);
+
+#endregion
+#region task2
+////Ədədlərdən ibarət olan arraydə neçə müsbət neçə mənfi ədəd olduğunu ekrana çıxardan alqoritm yazın.
+
+//int[] numberArr = { 1, 2, 3, -1, -2, -3 };
+//int numberOfPositive = 0;
+//int numberOfNegative = 0;
+
+//for (int i = 0; i < numberArr.Length; i++)
+//{
+//    if (numberArr[i] > 0)
+//    {
+//        numberOfPositive++;
+//    }
+//    else if (numberArr[i] < 0)
+//    {
+//        numberOfNegative++;
+//    }
+//}
+//Console.WriteLine("musbet ededlerin sayi: " + numberOfPositive);
+//Console.WriteLine("menfi ededlerin sayi: " + numberOfNegative);
+#endregion
+#region task3
+////Ədədlərdən ibarət olan bir array verilib həmin arraydə olan ən böyük ədədi ekrana yazdıran alqoritm yazın.
+
+//int[] numberArr = { -28, -1, 9, 12, -3 };
+//int greatestNumber = numberArr[0];
+
+//for (int i = 1; i < numberArr.Length; i++)
+//{
+//    if (numberArr[i] > greatestNumber)
+//    {
+//        greatestNumber= numberArr[i];
+//    }
+//}
+//Console.WriteLine(greatestNumber);
+#endregion
